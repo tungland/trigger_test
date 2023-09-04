bb40d2128187c6e428eb14dee1f2dcf0af84f7bd
Lars Magne Tungland <larsmtungland@gmail.com>
2023-09-04 15:44:46 +0200

    try adding something

--- main.py
+++ main.py
@@ -9,6 +9,10 @@ def hello():
     name = os.environ.get('NAME', 'World')
     return f'Lol {name}!'
 
+@app.route('/test')
+def test():
+    return 'Test and some other stuff'
+
 if __name__=="__main__":
     app.run(debug=True, host='0.0.0.0', port=int(os.environ.get('PORT', 8080)))
     
